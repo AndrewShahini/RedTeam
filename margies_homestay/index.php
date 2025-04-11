97620d754d2b97f05f995d9287887a45a3fb5046
florence <2380334@edu.vaniercollege.qc.ca>
2025-04-10 21:29:01 -0400

    start and progress homepage

---
+++ margies_homestay/index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="assets/styles/globals.css" />
+    <link rel="stylesheet" href="assets/styles/styleguide.css" />
+    <link rel="stylesheet" href="assets/styles/style.css" />
+  </head>
+  <body>
+    <div class="homepage-desktop">
+      <div class="div">
+        <img class="group" src="assets/images/activities_boats.jpg" />
+        <div class="overlap">
+          <div class="overlap-group-wrapper">
+            <div class="overlap-group">
+              <p class="post-title-here">
+                <span class="text-wrapper"
+                  >Nestled in the heart of a lush tropical paradise, this charming homestay in the Philippines offers a
+                  perfect blend of comfort and island-style living. Surrounded by swaying palm trees and vibrant
+                  gardens, the property features traditional Filipino architecture with modern amenities for a relaxing
+                  stay.<br
+                /></span>
+                <span class="span"><br /></span>
+              </p>
+              <p class="p">Cozy Tropical Homestay in <br />the Philippines</p>
+            </div>
+          </div>
+          <div class="book-now-btn-wrapper">
+            <div class="book-now-btn"><div class="post-title-here-2">BOOK NOW</div></div>
+          </div>
+          <img class="image" src="assets/images/sunset_beach.jpg" />
+        </div>
+        <div class="overlap-2">
+          <div class="rectangle"></div>
+          <div class="review-card">
+            <div class="rating">
+              <img class="star" src="img/star.svg" />
+              <img class="star" src="img/star.svg" />
+              <img class="star" src="img/star.svg" />
+              <img class="star" src="img/star.svg" />
+              <img class="star" src="img/star.svg" />
+            </div>
+            <div class="review-body">
+              <div class="text-heading"><div class="text-heading-2">Review title</div></div>
+              <div class="text"><div class="text-2">Review body</div></div>
+            </div>
+            <div class="avatar-block">
+              <div class="avatar"></div>
+              <div class="info">
+                <div class="title">Reviewer name</div>
+                <div class="description">Date</div>
+              </div>
+            </div>
+          </div>
+          <p class="reviews-see-all">
+            <span class="text-wrapper-2">Reviews<br /></span>
+            <span class="text-wrapper-3"><br /><br /><br /><br /></span>
+            <span class="text-wrapper-4">See all reviews</span>
+          </p>
+          <div class="review-card-2">
+            <div class="rating">
+              <img class="star" src="img/star.svg" />
+              <img class="star" src="img/star.svg" />
+              <img class="star" src="img/star.svg" />
+              <img class="star" src="img/star.svg" />
+              <img class="star" src="img/star.svg" />
+            </div>
+            <div class="review-body">
+              <div class="text-heading"><div class="text-heading-2">Review title</div></div>
+              <div class="text"><div class="text-2">Review body</div></div>
+            </div>
+            <div class="avatar-block">
+              <div class="avatar-2"></div>
+              <div class="info">
+                <div class="title">Reviewer name</div>
+                <div class="description">Date</div>
+              </div>
+            </div>
+          </div>
+          <div class="review-card-3">
+            <div class="rating">
+              <img class="star" src="img/star.svg" />
+              <img class="star" src="img/star.svg" />
+              <img class="star" src="img/star.svg" />
+              <img class="star" src="img/star.svg" />
+              <img class="star" src="img/star.svg" />
+            </div>
+            <div class="review-body">
+              <div class="text-heading"><div class="text-heading-2">Review title</div></div>
+              <div class="text"><div class="text-2">Review body</div></div>
+            </div>
+            <div class="avatar-block">
+              <div class="avatar-3"></div>
+              <div class="info">
+                <div class="title">Reviewer name</div>
+                <div class="description">Date</div>
+              </div>
+            </div>
+          </div>
+          <div class="review-card-4">
+            <div class="rating">
+              <img class="star" src="img/star.svg" />
+              <img class="star" src="img/star.svg" />
+              <img class="star" src="img/star.svg" />
+              <img class="star" src="img/star.svg" />
+              <img class="star" src="img/star.svg" />
+            </div>
+            <div class="review-body">
+              <div class="text-heading"><div class="text-heading-2">Review title</div></div>
+              <div class="text"><div class="text-2">Review body</div></div>
+            </div>
+            <div class="avatar-block">
+              <div class="avatar-4"></div>
+              <div class="info">
+                <div class="title">Reviewer name</div>
+                <div class="description">Date</div>
+              </div>
+            </div>
+          </div>
+          <div class="text-wrapper-5">Latest Reviews</div>
+        </div>
+        <div class="overlap-3">
+          <div class="group-2"></div>
+          <div class="group-2"></div>
+          <img class="vector" src="assets/images/globe.png" />
+          <img class="img" src="assets/images/viloria_homestay_logo_copy.png" />
+          <div class="text-wrapper-6">Booking</div>
+          <div class="text-wrapper-7">Policies</div>
+          <div class="text-wrapper-8">About us</div>
+          <button class="button">Sign in</button>
+          <button class="button-3">Register</button>
+        </div>
+        <div class="overlap-4">
+          <div class="text-wrapper-9">Inclusions</div>
+          <p class="text-wrapper-10">What we have to offer</p>
+          <div class="overlap-5">
+            <div class="group-3">
+              <div class="text-wrapper-11">Nearby Beach</div>
+              <div class="overlap-6">
+                <div class="text-wrapper-12">Free Parking</div>
+                <div class="text-wrapper-13">Good Wi-Fi</div>
+              </div>
+              <div class="text-wrapper-14">TV included</div>
+              <div class="overlap-7">
+                <p class="text-wrapper-15">Happy Vibes Only ! !</p>
+                <div class="text-wrapper-16">Fridge Included</div>
+              </div>
+              <img class="icon-pool" src="img/icon-pool.png" />
+              <img class="icon-food-bank" src="img/icon-food-bank.png" />
+              <img class="icon-wifi" src="img/icon-wifi.png" />
+              <div class="overlap-8">
+                <img class="icon-local-parking" src="img/icon-local-parking.png" />
+                <img class="game-icons-tv" src="img/game-icons-tv.svg" />
+              </div>
+              <img class="icon-emoji-emotions" src="img/icon-emoji-emotions.png" />
+            </div>
+            <p class="text-wrapper-17">Kitchen essentials included ! (pots, pans, utensils, ect)</p>
+            <div class="text-wrapper-18">Hot and cold showers</div>
+            <img class="knife-and-spatchula" src="img/knife-and-spatchula.png" />
+            <img class="shower" src="img/shower.png" />
+          </div>
+        </div>
+        <div class="overlap-9">
+          <div class="text-wrapper-19">Explore the area</div>
+          <div class="group-4">
+            <p class="element-km-away-from-the">
+              85 km away from the Laoag International airport <br />3 minutes away from the Saud Beach
+            </p>
+            <img class="location-on" src="img/location-on.svg" />
+            <p class="viloria-s-homestay">Viloria&#39;s Homestay, Pagudpud, 2919 Ilocos Norte, <br />Philippines</p>
+            <p class="element-km-away-from-the-2">
+              576 km away from the Ninoy Aquino International Airport (Manila) <br />169 km away from the Cagayan North
+              International
+            </p>
+          </div>
+          <div class="group-wrapper">
+            <div class="group-5">
+              <div class="overlap-10">
+                <img class="img-2" src="img/icon-add.png" /> <img class="icon-remove" src="img/icon-remove.png" />
+              </div>
+              <img class="img-2" src="img/icon-zoom-out-map.png" />
+            </div>
+          </div>
+        </div>
+        <div class="overlap-11">
+          <div class="rectangle-2"></div>
+          <!-- These should be anchor links -->
+          <div class="text-wrapper-20">Booking</div>
+          <div class="text-wrapper-21">Inclusions</div>
+          <div class="text-wrapper-22">Image carousel</div>
+          <div class="text-wrapper-23">explore the area</div>
+          <div class="text-wrapper-24">Reviews</div>
+          <img class="line" src="img/line-21.svg" />
+          <img class="line-2" src="img/line-22.svg" />
+          <p class="text-wrapper-25">© 2025 Valoria's Homestay. All rights deserved</p>
+          <div class="text-wrapper-26">About us</div>
+          <div class="text-wrapper-27">Policies</div>
+          <img class="image-2" src="assets/images/viloria_homestay_logo_copy.png" />
+          <div class="text-wrapper-28">Contact us</div>
+          <img class="facebook" src="assets/images/facebook-social-logo.svg" />
+          <div class="text-wrapper-29">margies@gmail.com</div>
+        </div>
+        <img class="line-3" src="img/line-23.svg" />
+      </div>
+    </div>
+  </body>
+</html>
